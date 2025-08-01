a35cf645afdba745384e89b4aba11aae92e70db4
trainghost <trainghost@Naver.com>
2025-08-01 14:26:47 +0900

    Update script.js

--- script.js
+++ script.js
@@ -8,15 +8,13 @@ function convertToTable() {
     return;
   }
   
-  let html = '<table><thead><tr><th>순위</th><th>이름</th><th>승</th><th>무</th><th>패</th><th>승점 (전체/평균)</th><th>득실</th><th>승률</th><th>참가 횟수</th></tr></thead><tbody>';
+  let html = '<table><thead><tr><th>순위</th><th>이름</th><th>참석</th><th>늦참</th><th>승</th><th>무</th><th>패</th><th>승점 (전체/평균)</th><th>득실</th><th>승률</th><th>참가 횟수</th></tr></thead><tbody>';
   
   lines.forEach(line => {
-    // 텍스트에서 불필요한 공백을 제거하고 슬래시(/)나 공백을 기준으로 데이터를 분리
     const data = line.trim().split(/\s*\/\s*|\s+/);
     
-    // 데이터가 충분한지 확인 (최소한의 컬럼 수)
     if (data.length >= 8) {
-      const rank = data[0].replace('.', ''); // 순위에서 점(.) 제거
+      const rank = data[0].replace('.', '');
       const name = data[1];
       const win = data[2];
       const draw = data[3];
@@ -29,6 +27,8 @@ function convertToTable() {
       html += `<tr>
                 <td>${rank}</td>
                 <td>${name}</td>
+                <td><input type="checkbox"></td>
+                <td><input type="checkbox"></td>
                 <td>${win}</td>
                 <td>${draw}</td>
                 <td>${lose}</td>
